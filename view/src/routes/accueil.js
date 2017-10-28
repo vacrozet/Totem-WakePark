da4b39ceb366fd072a4a998cdb8a4639b1b14cb3
vacrozet <vacrozet@student.42.fr>
2017-10-28 22:10:56 +0200

    ko

--- view/src/routes/accueil.js
+++ view/src/routes/accueil.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import Coverflow from 'react-coverflow'
+import { StyleRoot } from 'radium'
 
 class Accueil extends Component {
   render () {
     return (
       <div>
-        coucou accueil
+        <StyleRoot>
+          <Coverflow
+            displayQuantityOfSide={2}
+            navigation={true}
+            enableHeading={true}
+            media={{
+              '@media (max-width: 900px)': {
+                width: '600px',
+                height: '300px'
+              },
+              '@media (min-width: 900px)': {
+                width: '960px',
+                height: '600px'
+              }
+            }}
+            >
+            <img src='images/album-1.png' alt='Album one' data-action="https://facebook.github.io/react/"/>
+            <img src='images/album-2.png' alt='Album two' data-action="http://passer.cc"/>
+            <img src='images/album-3.png' alt='Album three' data-action="https://doce.cc/"/>
+            <img src='images/album-4.png' alt='Album four' data-action="http://tw.yahoo.com"/>
+          </Coverflow>
+        </StyleRoot>
       </div>
     )
   }
