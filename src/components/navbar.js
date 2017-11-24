d578668d1459eb51b33a489cd452efc9b247df83
vacrozet <vacrozet@student.42.fr>
2017-11-24 22:24:52 +0100

    navbar_ok

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import '../App.css'
 
 class Frontbarre extends Component {
   render () {
     return (
-      <div>
-        <Navbar fluid>
+      <div id='frontbarre'>
+        <Navbar inverse collapseOnSelect className='navbar'>
           <Navbar.Header>
             <Navbar.Brand>
               <a href='#'>Totem Wake Park</a>
             </Navbar.Brand>
+            <Navbar.Toggle />
           </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href='#'>Link</NavItem>
-            <NavItem eventKey={2} href='#'>Link</NavItem>
-            <NavDropdown eventKey={3} title='Dropdown' id='basic-nav-dropdown'>
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href='#'>home</NavItem>
+              <NavItem eventKey={2} href='#'>about</NavItem>
+              <NavItem eventKey={3} href='#'>products</NavItem>
+              <NavItem eventKey={4} href='#'>services</NavItem>
+              <NavDropdown eventKey={5} title='Dropdown' id='basic-nav-dropdown'>
+                <MenuItem eventKey={5.1}>Action</MenuItem>
+                <MenuItem eventKey={5.2}>Another action</MenuItem>
+                <MenuItem eventKey={5.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={5.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
-import Index from './App'
+import ReactDOM from 'react-dom'
+import React, { Component } from 'react'
+import Frontbarre from './components/navbar.js'
 import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import Home from './routes/home'
 
-ReactDOM.render(<Index />,
-    document.getElementById('root'))
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Frontbarre id='navbar' history={this.props.history} match={this.props.match} location={this.props.location} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route component={Index} />
+  </BrowserRouter>, document.getElementById('root'))
 registerServiceWorker()
