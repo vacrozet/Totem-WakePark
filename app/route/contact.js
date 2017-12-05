c3e785519966210e8e8232e23322ec789d8ec399
vacrozet <vacrozet@student.42.fr>
2017-12-05 22:25:24 +0100

    contact

--- app/route/contact.js
+++ app/route/contact.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.put('/message', require('../controleur/contact/sendMessage.js'))
+// router.put('/message', require('../controleur/contact/sendMessage.js'))
 
 module.exports = router
