8e2f6bf32644929b19a0aac2c36c968ef3b375a0
vacrozet <vacrozet@student.42.fr>
2017-12-06 18:03:23 +0100

    first

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import { ControlLabel, FormGroup, FormControl, Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon, Modal, Button } from 'react-bootstrap'
 import '../App.css'
 import { Link } from 'react-router-dom'
 
@@ -7,6 +7,9 @@ class Frontbarre extends Component {
   constructor (props) {
     super(props)
     this.state = {
+      show: true,
+      login: '',
+      password: ''
     }
     this.handleChangePage = this.handleChangePage.bind(this)
   }
@@ -14,9 +17,38 @@ class Frontbarre extends Component {
   handleChangePage (page) {
     this.props.history.push(page)
   }
+  handleChange (evt) {
+    this.setState({[evt.target.name]: evt.target.value})
+  }
+
+  handleModalConnexion () {
+    return (
+      <Modal
+        show={this.state.show}
+        onHide={() => this.setState({show: false})}
+        container={this}
+        aria-labelledby='contained-modal-title'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id='contained-modal-title'>Connexion</Modal.Title>
+        </Modal.Header>
+        <FormGroup>
+          <ControlLabel>Login</ControlLabel>
+          <FormControl name='login' type='text' placeholder='Login' value={this.state.writeComment} onChange={this.handleChange.bind(this)} />
+          <ControlLabel>Password</ControlLabel>
+          <FormControl name='password' type='text' placeholder='Password' value={this.state.writeComment} onChange={this.handleChange.bind(this)} />
+        </FormGroup>
+        <Modal.Footer>
+          <Button onClick={() => this.setState({show: false})}>Close</Button>
+          <Button bsStyle='primary' onClick={() => this.setState({show: false})}>Connexion</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
   render () {
     return (
       <div id='frontbarre'>
+        {this.handleModalConnexion()}
         <Navbar inverse collapseOnSelect className='navbar'>
           <Navbar.Header>
             <Navbar.Brand>
@@ -35,7 +67,7 @@ class Frontbarre extends Component {
                 <MenuItem eventKey={5.2}>Another action</MenuItem>
                 <MenuItem eventKey={5.3}>Something else here</MenuItem>
                 <MenuItem divider />
-                <MenuItem eventKey={5.3}>Separated link</MenuItem>
+                <MenuItem eventKey={5.3} onClick={() => this.setState({show: true})}><Glyphicon glyph='cog' /> Settings</MenuItem>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
