59c4d58ea073e7ee8febbcff017055037e58d605
Valentin CROZET <vacrozet@e2r5p12.42.fr>
2017-12-13 17:35:46 +0100

    weather KO

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import { local } from '../utils/api.js'
-// import ReactWeather from 'react-open-weather'
-var Forecast = require('react-forecast')
 
 class Weather extends Component {
   constructor (props) {
@@ -23,14 +21,7 @@ class Weather extends Component {
   render () {
     return (
       <div>
-        <Forecast latitude={34.05} longitude={118.25} name='Los Angeles' />
-        <Component />
-        {/* <ReactWeather
-          forecast='today'
-          apikey='60a98d92084e415eb0e162324170611'
-          type='city'
-          city='Creches-sur-Saone'
-        /> */}
+        coucou
       </div>
     )
   }
