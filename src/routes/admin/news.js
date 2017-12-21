84d71055fc09a71ef1bbc50002672eca1d784fb0
vacrozet <vacrozet@student.42.fr>
2017-12-22 00:07:50 +0100

    nes

--- src/routes/admin/news.js
+++ src/routes/admin/news.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import bcrypt from 'bcryptjs'
+import Dropzone from 'react-dropzone'
+import { local } from '../../utils/api.js'
 import { FormGroup, FormControl, Button } from 'react-bootstrap'
 
 class News extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      name: ''
+      name: '',
+      type: ['INFO IMPORTANTE', 'SOIRÉE', 'PROMOTION'],
+      text: ''
     }
   }
 
@@ -19,7 +23,55 @@ class News extends Component {
       }
     }
   }
-
+  onDrop (acceptedFiles, rejectedFiles) {
+    acceptedFiles.forEach(file => {
+      let type
+      if (file.type === 'image/jpeg') {
+        type = '.jpg'
+      } else {
+        type = '.png'
+      }
+      var reader = new global.FileReader()
+      reader.readAsDataURL(file)
+      reader.onload = () => {
+        local().put('/picture/picture', {
+          pic: reader.result,
+          name: file.name,
+          type: type,
+          dirName: this.state.dirName
+        }).then((res) => {
+          if (res.data.success === true) {
+            this.props.notification.addNotification({
+              message: res.data.message,
+              level: 'success'
+            })
+            this.setState({isLoading: false})
+            local().get('/picture/album', {
+              params: {
+                dir: this.state.dirName
+              }
+            }).then((res1) => {
+              if (res1.data.success === true) {
+                this.setState({
+                  album: res1.data.result[0].pictures,
+                  albumName: res1.data.result[0]._id
+                })
+              }
+              this.setState({ isLoading: false })
+            }).catch((err) => { console.log(err.response) })
+          }
+        }).catch((err) => {
+          if (err.response) console.log(err.response.data.error)
+        })
+      }
+      reader.onerror = function (error) {
+        console.log('Error when reading image: ', error)
+      }
+    })
+    if (rejectedFiles.length !== 0) {
+      console.log('This file is not allowed please use png < 1mb, error')
+    }
+  }
   handleChange (evt) {
     this.setState({[evt.target.name]: evt.target.value})
   }
@@ -29,12 +81,21 @@ class News extends Component {
       <div>
         <div id='formulaire'>
           <h1>Créer une Actu</h1>
-          <FormGroup bsSize='small'>
+          <FormGroup bsSize='small' className='barre'>
+            <FormControl
+              componentClass='select'
+              placeholder='select'
+              onChange={this.handleChange.bind(this)}>
+              <option value=''>Type D'évènement</option>
+              {this.state.type.map((tab, index) => {
+                return (<option key={index} value={tab}>{tab}</option>)
+              })}
+            </FormControl>
             <FormControl
               className='inputform'
               name='name'
               type='text'
-              placeholder='evenement'
+              placeholder={'Nom de l\'évènement'}
               value={this.state.createDir}
               onChange={this.handleChange.bind(this)}
             />
@@ -59,7 +120,34 @@ class News extends Component {
                 Créer
             </Button>
             )}
+            <center><h3>Texte</h3></center>
+            <FormControl
+              id='resize'
+              componentClass='textarea'
+              type='textaera'
+              placeholder='Saisir le texte'
+              name='text'
+              onChange={this.handleChange.bind(this)}
+              />
+            <h3>Image</h3>
+            <Dropzone
+              disablePreview
+              className='buttonformimage'
+              accept='image/png, image/jpeg'
+              maxSize={10000000000}
+              onDrop={this.onDrop.bind(this)}>
+              <Button
+                className='buttonformimage'
+                bsStyle='primary'
+                name='submit'
+              >
+            Charger une Image
+            </Button>
+            </Dropzone>
           </FormGroup>
+          <div className='imagePreview' >
+          coucou
+          </div>
         </div>
       </div>
     )
